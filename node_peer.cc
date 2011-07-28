d4d5a306d4fe0f80a47a66f56729394b0c06c7e5
Zac Morris <zac@caffeinatedmind.com>
2011-07-27 18:45:34 -0700

    Beginning modifications to holepoke library to work with new node architecture, redefining holepoke communication in holepoke.proto

---
+++ node_peer.cc
@@ -0,0 +1,9 @@
+//
+//  node_peer.cc
+//  Expresso-Node
+//
+//  Created by zac on 7/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ node_peer.h
@@ -0,0 +1,14 @@
+//
+//  node_peer.h
+//  Expresso-Node
+//
+//  Created by zac on 7/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Expresso_Node_node_peer_h
+#define Expresso_Node_node_peer_h
+
+
+
+#endif
